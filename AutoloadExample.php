23b8ab2879916b3e3e7cd417b1bc4a22530398be
wallsblack <smartfluff.com@gmail.com>
2018-01-24 03:21:34 +0200

    test

--- AutoloadExample.php
+++ AutoloadExample.php
@@ -2,13 +2,32 @@
 
 namespace wallsblack\blockchain;
 
+use yii\base\Widget;
+use Blockchain\Blockchain;
+
 /**
  * This is just an example.
  */
-class AutoloadExample extends \yii\base\Widget
+class Rates extends Widget
 {
+    public $cur;
+    public $api_code = null;
+
+    public function init()
+    {
+        parent::init();
+
+
+        if ($this->cur === null) {
+            $this->cur = 'USD';
+        }
+    }
+
     public function run()
     {
-        return "Hello!";
+        $blockchain = new Blockchain($this->api_code);
+        $rates = $blockchain->Rates->get();
+
+        return $rates;
     }
 }
